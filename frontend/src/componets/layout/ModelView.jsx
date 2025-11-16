f5575fb8ee304a7f7278f9051c93d46fdede2ac7
Elvismn <mongerielvis@gmail.com>
2025-11-16 15:40:53 +0300

    Added new layout features

--- frontend/src/componets/layout/ModelView.jsx
+++ frontend/src/componets/layout/ModelView.jsx
@@ -1,3 +1,4 @@
+// Updated ModelView.jsx
 import React from 'react';
 import ParentsView from '../models/ParentsView';
 import StaffView from '../models/StaffView';
@@ -9,19 +10,25 @@ import StakeholdersView from '../models/StakeholdersView';
 import InventoryView from '../models/InventoryView';
 import CurriculumView from '../models/CurriculumView';
 import StudentsView from '../models/StudentsView';
-const ModelView = ({ model }) => {
+
+const ModelView = ({ model, searchTerm, searchResults }) => {
   const renderModelContent = () => {
+    const commonProps = {
+      searchTerm: searchTerm,
+      searchResults: searchResults
+    };
+
     switch (model) {
-      case 'parents': return <ParentsView />;
-      case 'students': return <StudentsView/>
-      case 'staff': return <StaffView />;
-      case 'courses': return <CoursesView />;
-      case 'classrooms': return <ClassroomsView />;
-      case 'clubs': return <ClubsView />;
-      case 'departments': return <DepartmentsView />;
-      case 'stakeholders': return <StakeholdersView />;
-      case 'inventory': return <InventoryView />;
-      case 'curriculum': return <CurriculumView />;
+      case 'parents': return <ParentsView {...commonProps} />;
+      case 'students': return <StudentsView {...commonProps} />;
+      case 'staff': return <StaffView {...commonProps} />;
+      case 'courses': return <CoursesView {...commonProps} />;
+      case 'classrooms': return <ClassroomsView {...commonProps} />;
+      case 'clubs': return <ClubsView {...commonProps} />;
+      case 'departments': return <DepartmentsView {...commonProps} />;
+      case 'stakeholders': return <StakeholdersView {...commonProps} />;
+      case 'inventory': return <InventoryView {...commonProps} />;
+      case 'curriculum': return <CurriculumView {...commonProps} />;
       default: return (
         <div className="text-center py-12">
           <h3 className="text-xl font-semibold text-gray-600">
